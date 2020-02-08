a8a22e27393382fd9e225d1e9e35f3e3d429552c
John <john.yalda@hotmail.com>
2020-02-08 14:02:31 -0500

    basic webpack setup using webpack.config file

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.export = {
+  mode: 'development',
+  entry: ".\\src\\index.js",
+  output: {
+    path: path.resolve(__dirname, ".\\dist"),
+    filename: 'main.js'
+  }
+}
